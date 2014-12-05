7c4bf1c70b0d1bb4ae8134d8a852f2166b263295
kiof <kiof@free.fr>
2014-12-05 20:09:49 +0100

    New Admob SDK

--- boussole/build/generated/source/buildConfig/debug/com/kiof/boussole/BuildConfig.java
+++ boussole/build/generated/source/buildConfig/debug/com/kiof/boussole/BuildConfig.java
@@ -5,9 +5,9 @@ package com.kiof.boussole;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.kiof.boussole";
+  public static final String APPLICATION_ID = "com.kiof.boussole";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "1.2";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.3";
 }
